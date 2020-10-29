ccdd81bd17c1af114d0d86d17ff9a22d68f095aa
Celeste <cecuercu@gmail.com>
2020-10-29 13:56:30 +0100

    Tarea 2: Wait, Notify por Join

---
+++ src/es/centroafuera/psp/ejemplosJava/Cliente.java
@@ -0,0 +1,21 @@
+package es.centroafuera.psp.ejemplosJava;
+
+public class Cliente extends Thread {
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                if (Main.list.isEmpty()){
+                    System.out.println("Cliente - Voy a esperar a que me traigan comida...");
+                    break;
+                } else {
+                    System.out.println("Cliente - Voy a consumir un " + Main.list.get(0) + " 3 segundos");
+                    Main.list.remove(0);
+                    sleep(3000);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/es/centroafuera/psp/ejemplosJava/Main.java
@@ -0,0 +1,23 @@
+package es.centroafuera.psp.ejemplosJava;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static final ArrayList<String> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+            try {
+                while (true) {
+                    Cliente cliente = new Cliente();
+                    Camarero camarero = new Camarero();
+                    camarero.start();
+                    camarero.join();
+                    cliente.start();
+                    cliente.join();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+    }
+}
