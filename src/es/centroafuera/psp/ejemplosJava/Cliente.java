b7ffe1f63f88d8cf463e122a0681f945aa1c2b9f
Celeste <celescc96@hotmail.com>
2020-11-03 14:05:30 +0100

    Tarea 2: Wait, Notify por Join

--- src/es/centroafuera/psp/ejemplosJava/Cliente.java
+++ src/es/centroafuera/psp/ejemplosJava/Cliente.java
@@ -4,16 +4,14 @@ public class Cliente extends Thread {
     @Override
     public void run() {
         try {
-            while (true) {
-                if (Main.list.isEmpty()){
-                    System.out.println("Cliente - Voy a esperar a que me traigan comida...");
-                    break;
-                } else {
-                    System.out.println("Cliente - Voy a consumir un " + Main.list.get(0) + " 3 segundos");
-                    Main.list.remove(0);
-                    sleep(3000);
-                }
+            while (!Main.list.isEmpty()){
+                System.out.println("Cliente - Voy a consumir un " + Main.list.get(0) + " 3 segundos");
+                Main.list.remove(0);
+                sleep(3000);
             }
+
+            System.out.println("Cliente - Voy a esperar a que me traigan comida...");
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
